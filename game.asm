5929fe84b2c57a45345b5e8c1c5bad71dd6b979e
= <=>
2025-12-06 22:33:22 +0100

    Using video

--- game.asm
+++ game.asm
@@ -1,7 +1,28 @@
 org 0x7C00
 
-cli
-hlt
+;; Video Mode and Video Memory
+
+mov ax, 0x0013
+int 0x10
+
+push 0x0A000
+pop es
+
+;; Main Game Loop
+
+game_loop:
+    mov al, 0x04
+    mov cx, 320*200
+
+    xor di, di
+    rep stosb
+
+jmp game_loop
+
+game_cleanup:
+    cli
+    hlt
 
 section boot_section start=0x7DFE
+
 dw 0xAA55
